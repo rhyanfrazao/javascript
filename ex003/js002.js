c15086d6d3e52c8544cd8ab013983afc6e0ba7be
Rhyan Domingues Damasceno Frazão <139800455+rhyanfrazao@users.noreply.github.com>
2023-11-01 10:16:46 -0300

    convertendo variaveis

--- ex003/js002.js
+++
@@ -1,5 +0,0 @@
-// window.alert("Insira sua mensagem ao usuário aqui");
-// window.confirm("Confirme seu desejo de continuar na página");
-
-var nome = window.prompt("Qual o seu nome?");
-window.alert("É um prazer te conhecer, " + nome + "!");
---
+++ ex003/js003.js
@@ -0,0 +1,5 @@
+var n1 = Number.parseFloat(window.prompt("Digite o primeiro número: "));
+var n2 = Number.parseFloat(window.prompt("Digite o segundo número: "));
+var soma = n1 + n2;
+// window.alert("A soma dos valores é: " + soma);
+window.alert(`A soma entre ${n1} e ${n2} é igual a ${soma}`);
