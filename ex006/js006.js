f9731c878ea6b81ad2b425cc37ce0e00da26852d
Rhyan Domingues Damasceno Frazão <139800455+rhyanfrazao@users.noreply.github.com>
2023-11-03 12:12:55 -0300

    elementos por id

---
+++ ex006/js006.js
@@ -0,0 +1,16 @@
+function clicar(){
+    var a = window.document.getElementById('area');
+    a.innerText = "Clicou";
+    a.style.backgroundColor = "red";
+}
+
+function entrar(){
+    var a = window.document.getElementById('area');
+    a.innerText = "Entrou";
+}
+
+function sair(){
+    var a = window.document.getElementById('area');
+    a.innerText = "Saiu";
+    a.style.backgroundColor = "green";
+}
