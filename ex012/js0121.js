6d233dc8c3e0cd411025f0cea3a629ad82319699
Rhyan Domingues Damasceno Frazão <139800455+rhyanfrazao@users.noreply.github.com>
2024-04-17 09:05:27 -0300

    Create js0121.js

---
+++ ex012/js0121.js
@@ -0,0 +1,18 @@
+var cont = 1
+
+// while (cont <= 7){
+//     console.log(`Gol ${cont} da Alemanha`)
+//     cont++
+// }
+// console.log('Gol do Brasil')
+
+// do{
+//     console.log(`Gol ${cont} da Alemanha`)
+//     cont++
+// }while (cont <= 7)
+// console.log('Gol do Brasil')
+
+for (var cont = 1; cont <= 7; cont++){
+    console.log(`Gol ${cont} da Alemanha`)
+}
+console.log('Gol do Brasil')
